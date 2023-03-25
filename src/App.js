1ae2af0889c31ad922fa1116b0faa180465d2436
Rory Donald <rory.alexander.donald@gmail.com>
2023-03-25 01:26:19 -0600

    Added dummy gauge chart component from highcharts.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,106 @@ import neutralGIF from './images/neutralGIF.gif';
 import React, { useState, useEffect } from 'react';
 import LiveStreamingServices from "./services/livestreamServices.js";
 import Sentiment from 'sentiment';
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official';
 
 const sentiment = new Sentiment();
 const chatMesages = new Set();
 const APIKey = "<API_KEY_HERE>";
 
+const options = {
+
+  chart: {
+      type: 'gauge',
+      plotBackgroundColor: null,
+      plotBackgroundImage: null,
+      plotBorderWidth: 0,
+      plotShadow: false,
+      height: '80%'
+  },
+
+  title: {
+      text: 'Speedometer'
+  },
+
+  pane: {
+      startAngle: -90,
+      endAngle: 89.9,
+      background: null,
+      center: ['50%', '75%'],
+      size: '110%'
+  },
+
+  // the value axis
+  yAxis: {
+      min: 0,
+      max: 200,
+      tickPixelInterval: 72,
+      tickPosition: 'inside',
+      tickColor: Highcharts.defaultOptions.chart.backgroundColor || '#FFFFFF',
+      tickLength: 20,
+      tickWidth: 2,
+      minorTickInterval: null,
+      labels: {
+          distance: 20,
+          style: {
+              fontSize: '14px'
+          }
+      },
+      plotBands: [{
+          from: 0,
+          to: 120,
+          color: '#55BF3B', // green
+          thickness: 20
+      }, {
+          from: 120,
+          to: 160,
+          color: '#DDDF0D', // yellow
+          thickness: 20
+      }, {
+          from: 160,
+          to: 200,
+          color: '#DF5353', // red
+          thickness: 20
+      }]
+  },
+
+  series: [{
+      name: 'Speed',
+      data: [80],
+      tooltip: {
+          valueSuffix: ' km/h'
+      },
+      dataLabels: {
+          format: '{y} km/h',
+          borderWidth: 0,
+          color: (
+              Highcharts.defaultOptions.title &&
+              Highcharts.defaultOptions.title.style &&
+              Highcharts.defaultOptions.title.style.color
+          ) || '#333333',
+          style: {
+              fontSize: '16px'
+          }
+      },
+      dial: {
+          radius: '80%',
+          backgroundColor: 'gray',
+          baseWidth: 12,
+          baseLength: '0%',
+          rearLength: '0%'
+      },
+      pivot: {
+          backgroundColor: 'gray',
+          radius: 6
+      }
+  }]
+}
+
 function App() {
+  var Highcharts = require('highcharts');
+  require('highcharts/highcharts-more')(Highcharts);
+
   const [streamURL, setURL] = useState('');
   const [sentimentScore, setSentimentScore] = useState(null);
 
@@ -51,6 +145,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h2>Sentiment Analysis</h2>
+
+        <HighchartsReact highcharts={Highcharts} options={options}></HighchartsReact>
 
         <input value={streamURL} onChange={e => setURL(e.target.value)} style={{ padding: '20px', fontSize: '20px', width: '90%' }}/>
 
