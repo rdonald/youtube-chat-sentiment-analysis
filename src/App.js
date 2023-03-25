c339e965f51f9db74dd534097ef3198c06bc6eae
Rory Donald <rory.alexander.donald@gmail.com>
2023-03-25 13:59:22 -0600

    Imported highcharts.js and is updating in real time with API requests

--- src/App.js
+++ src/App.js
@@ -13,33 +13,39 @@ const sentiment = new Sentiment();
 const chatMesages = new Set();
 const APIKey = "<API_KEY_HERE>";
 
-const options = {
+function App() {
+  var Highcharts = require('highcharts');
+  require('highcharts/highcharts-more')(Highcharts);
 
-  chart: {
+  const [streamURL, setURL] = useState('');
+  const [sentimentScore, setSentimentScore] = useState(null);
+
+  const [chartOptions, setChartOptions] = useState ({
+    chart: {
       type: 'gauge',
       plotBackgroundColor: null,
       plotBackgroundImage: null,
       plotBorderWidth: 0,
       plotShadow: false,
       height: '80%'
-  },
-
-  title: {
-      text: 'Speedometer'
-  },
-
-  pane: {
+    },
+  
+    title: {
+      text: 'Chat Sentiment Score'
+    },
+  
+    pane: {
       startAngle: -90,
       endAngle: 89.9,
       background: null,
       center: ['50%', '75%'],
       size: '110%'
-  },
-
-  // the value axis
-  yAxis: {
-      min: 0,
-      max: 200,
+    },
+  
+    // the value axis
+    yAxis: {
+      min: -100,
+      max: 100,
       tickPixelInterval: 72,
       tickPosition: 'inside',
       tickColor: Highcharts.defaultOptions.chart.backgroundColor || '#FFFFFF',
@@ -47,67 +53,66 @@ const options = {
       tickWidth: 2,
       minorTickInterval: null,
       labels: {
-          distance: 20,
-          style: {
-              fontSize: '14px'
-          }
+        distance: 20,
+        style: {
+          fontSize: '14px'
+        }
       },
-      plotBands: [{
-          from: 0,
-          to: 120,
-          color: '#55BF3B', // green
-          thickness: 20
-      }, {
-          from: 120,
-          to: 160,
-          color: '#DDDF0D', // yellow
-          thickness: 20
-      }, {
-          from: 160,
-          to: 200,
+      plotBands: [
+        {
+          from: -100,
+          to: -40,
           color: '#DF5353', // red
           thickness: 20
-      }]
-  },
-
-  series: [{
-      name: 'Speed',
-      data: [80],
-      tooltip: {
-          valueSuffix: ' km/h'
-      },
+        },
+        {
+          from: -40,
+          to: 0,
+          color: '#ffcccb', // light red
+          thickness: 20
+        },
+        {
+          from: -0,
+          to: 40,
+          color: '#90EE90', // light green
+          thickness: 20
+        },
+        {
+          from: 40,
+          to: 100,
+          color: '#55BF3B', // green
+          thickness: 20
+        }]
+    },
+  
+    series: [{
+      name: 'Sentiment Score',
+      data: [0],
       dataLabels: {
-          format: '{y} km/h',
-          borderWidth: 0,
-          color: (
-              Highcharts.defaultOptions.title &&
-              Highcharts.defaultOptions.title.style &&
-              Highcharts.defaultOptions.title.style.color
-          ) || '#333333',
-          style: {
-              fontSize: '16px'
-          }
+        format: 'Sentiment Score: {y}',
+        borderWidth: 0,
+        color: (
+          Highcharts.defaultOptions.title &&
+          Highcharts.defaultOptions.title.style &&
+          Highcharts.defaultOptions.title.style.color
+        ) || '#333333',
+        style: {
+          fontSize: '16px'
+        }
       },
       dial: {
-          radius: '80%',
-          backgroundColor: 'gray',
-          baseWidth: 12,
-          baseLength: '0%',
-          rearLength: '0%'
+        radius: '80%',
+        backgroundColor: 'gray',
+        baseWidth: 12,
+        baseLength: '0%',
+        rearLength: '0%'
       },
       pivot: {
-          backgroundColor: 'gray',
-          radius: 6
+        backgroundColor: 'gray',
+        radius: 6
       }
-  }]
-}
-
-function App() {
-  var Highcharts = require('highcharts');
-  require('highcharts/highcharts-more')(Highcharts);
-
-  const [streamURL, setURL] = useState('');
-  const [sentimentScore, setSentimentScore] = useState(null);
+    }]
+  });
 
   useEffect(() => {
 
@@ -130,13 +135,19 @@ function App() {
         });
 
         setSentimentScore(sentimentIterationScore);
+
+        setChartOptions({
+          series: {
+            data: [sentimentIterationScore],
+          },
+        });
       }
 
       // Get new Chat Messages every 5 seconds
       const intervalId = setInterval(() => {
         getChatMessages(streamURL, APIKey, chatMesages)
       }, 1000 * 5) // in milliseconds
-
+      
       return () => clearInterval(intervalId)
     }
   }, [streamURL]);
@@ -146,9 +157,9 @@ function App() {
       <header className="App-header">
         <h2>Sentiment Analysis</h2>
 
-        <HighchartsReact highcharts={Highcharts} options={options}></HighchartsReact>
+        <HighchartsReact highcharts={Highcharts} options={chartOptions}></HighchartsReact>
 
-        <input value={streamURL} onChange={e => setURL(e.target.value)} style={{ padding: '20px', fontSize: '20px', width: '90%' }}/>
+        <input value={streamURL} onChange={e => setURL(e.target.value)} style={{ padding: '20px', fontSize: '20px', width: '90%' }} />
 
         {
           sentimentScore !== null ? <p>Sentiment Score: {sentimentScore}</p> : ''
