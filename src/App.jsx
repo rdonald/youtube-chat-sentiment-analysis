69488de66d16b9461cd2dcf07e7657b478902eba
Rory Donald <rory.alexander.donald@gmail.com>
2023-04-05 20:07:47 -0600

    Removed dead code

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,12 @@ import SentimentChartOptions from './highcharts/sentimentChartOptions.js'
 
 const sentiment = new Sentiment();
 const chatMesages = new Set();
-const negativeChatters = new Set();
-const positiveChatters = new Set();
 const APIKey = "<API_KEY_HERE>";
 const URL = prompt('Paste Stream URL:');
 
 function App() {
 
-  embed.setSize("1000px", "600px");
+  embed.setSize("1000px", "800px");
   var Highcharts = require('highcharts');
   require('highcharts/highcharts-more')(Highcharts);
 
@@ -39,13 +37,6 @@ function App() {
       // Iterate over each chat message and analyze it for sentiment analysis
       chatMesages.forEach((message) => {
         console.log(message);
-        if (sentiment.analyze(message).score < 0 && !negativeChatters.has(message)) {
-          document.getElementById("negativeChatters").innerHTML += ("<p>" + message + "</p>");
-          negativeChatters.add(message);
-        } else if (sentiment.analyze(message).score > 0 && !positiveChatters.has(message)) {
-          document.getElementById("positiveChatters").innerHTML += ("<p>" + message + "</p>");
-          positiveChatters.add(message);
-        }
         sentimentIterationScore += sentiment.analyze(message).score;
       });
 
@@ -76,12 +67,6 @@ function App() {
   return (
     <div className="App">
         <HighchartsReact highcharts={Highcharts} options={chartOptions}></HighchartsReact>
-        <div id="negativeChatters">
-          <h3 class="header">Malders</h3>
-        </div>
-        <div id="positiveChatters">
-          <h3 class="header">Poggers</h3>
-        </div>
     </div>
   );
 }
